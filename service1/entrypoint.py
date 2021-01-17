3152f680cd8449da00abc8be4bb38983bec493e3
iskragasparic <75320770+iskragasparic@users.noreply.github.com>
2021-01-17 23:24:43 +0100

    Update entrypoint.py

--- service1/entrypoint.py
+++ service1/entrypoint.py
@@ -5,16 +5,16 @@ import sys
 import hashlib
 
 #inp = sys.stdin.readline()  
-#inp = "testni primjer"
-inp = []
-while True:
-    input = sys.stdin.readline()
-    if input == '\n':
-        break
-    inp.append(input)
+inp = "testni primjer"
+#inp = []
+#while True:
+#    input = sys.stdin.readline()
+#    if input == '\n':
+#        break
+#    inp.append(input)
 
-#hash_func = "md5"
-hash_func = inp[0].strip()  #ukloni viska razmake s pocetka i kraja
+hash_func = "md5"
+#hash_func = inp[0].strip()  #ukloni viska razmake s pocetka i kraja
 message = ('\n'.join(inp)).strip() 
 h = hashlib.new(hash_func)  
 h.update(str.encode(message))  #str.enc... je element klase 'bytes' tocno kako nam treba
